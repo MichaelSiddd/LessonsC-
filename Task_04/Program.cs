a3e1798bba30007f4f3cff84b5e6f2e988ffd2a3
Michael <michael_siddd@icloud.com>
2022-06-27 19:36:06 +0300

    Add Task_04

--- Task_04/Program.cs
+++ Task_04/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа
+//  и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("!Программа определяющая максимальное целое число!");
+Console.WriteLine("Введите первое целое число");
+int numOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int numTwo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье целое число");
+int numThree = Convert.ToInt32(Console.ReadLine());
+int max = numOne;
+
+if (numTwo > max && numTwo > numThree) {
+    max = numTwo;
+    Console.WriteLine("Максимальное число = " + max);
+}
+else if (numThree > max && numThree > numTwo) {
+    max = numThree;
+    Console.WriteLine("Максимальное число = " + max);
+}
+else 
+{
+    Console.WriteLine("Максимальное число = " + max);
+}
