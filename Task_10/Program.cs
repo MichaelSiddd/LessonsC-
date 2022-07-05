18e08af161489d2cf6c3e01c895b6c42d981bf81
Michael <michael_siddd@icloud.com>
2022-07-05 20:34:32 +0300

    Add Task_10

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите целое трехзначное число");
+int rndNum = Convert.ToInt32(Console.ReadLine());
+
+int secondDigit = (rndNum % 100) / 10;
+
+Console.WriteLine("Его второе число = " + secondDigit);
+
+
+
+
+
+
+// Console.WriteLine($"Случайное трехзначное число  => {rndNum}");
+
+
+
